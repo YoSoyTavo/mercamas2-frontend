2b0e14fea3e2e8e63d43334022c7b4815f986131
DESKTOP-5CNLFR8\Windows 10 <ark.gustavomoreno@gmail.com>
2021-11-22 17:41:03 -0500

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { TablaProductosComponent } from './components/tabla-productos/tabla-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'productos', component: TablaProductosComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'iniciar sesion', component: IniciarSesionComponent},
+  {path: 'carrito', component: CarritoComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TablaProductosComponent } from './components/tabla-productos/tabla-productos.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TablaProductosComponent,
+    NotFoundComponent,
+    RegistroComponent,
+    CarritoComponent,
+    IniciarSesionComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
